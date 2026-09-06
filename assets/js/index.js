62b472994bf6807cfae887c07f1eae83ddb58216
Glakxic <ogglakxic@gmail.com>
2026-09-06 20:24:17 +0200

    JoinUs Page HTML / CSS done + reorganized js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,8 @@
+// --- Import ---
+import { error415 } from "./modules/error.js";
+
+// --- Function calling ---
 const viewport = window.innerWidth;
 if (viewport <= 768) {
   error415();
-}
-
-function error415() {
-  const page = `
-  <body>
-    <main>
-      <h1>Erreur 415 !</h1>
-      <p>Ce site est conçu pour être visiter sur un ordinateur</p>
-
-      <style>
-        @import url("https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap");
-        * {
-          margin: 0;
-          padding: 0;
-        }
-        html {
-          background-color: #303030;
-          color: #fff;
-          font-family: "Black Ops One", system-ui;
-          font-style: normal;
-          font-size: large;
-        }
-          
-        main {
-          display: flex;
-          flex-wrap: wrap;
-          flex-direction: column;
-          justify-content: center;
-          text-align: center;
-          align-items: center;
-          height: 100dvh;
-          
-        }
-      </style>
-    </main>
-  </body>
-  `;
-  return (document.querySelector("body").innerHTML = page);
 }
---
+++ assets/js/modules/error.js
@@ -0,0 +1,37 @@
+export function error415() {
+      const page = `
+  <body>
+    <main>
+      <h1>Erreur 415 !</h1>
+      <p>Ce site est conçu pour être visiter sur un ordinateur</p>
+
+      <style>
+        @import url("https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap");
+        * {
+          margin: 0;
+          padding: 0;
+        }
+        html {
+          background-color: #303030;
+          color: #fff;
+          font-family: "Black Ops One", system-ui;
+          font-style: normal;
+          font-size: large;
+        }
+          
+        main {
+          display: flex;
+          flex-wrap: wrap;
+          flex-direction: column;
+          justify-content: center;
+          text-align: center;
+          align-items: center;
+          height: 100dvh;
+          
+        }
+      </style>
+    </main>
+  </body>
+  `;
+    return (document.querySelector("body").innerHTML = page);
+}
